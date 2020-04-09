3264b037de23c1eb67a6eff999e1de61fb695fd8
Andre Ward <andre.ward62@gmail.com>
2020-04-08 23:16:33 -0300

    signup, login

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 
+const session = require("express-session"); 
+const bcrypt = require("bcrypt"); 
+const passport = require("passport"); 
+const LocalStrategy = require("passport-local").Strategy;
+
+const flash = require("connect-flash");
+
 mongoose
   .connect('mongodb://localhost/passport-roles', {
     useNewUrlParser: true,
@@ -23,11 +30,56 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
+// Mongoose configuration
+mongoose.Promise = Promise;
+mongoose
+  .connect('mongodb://localhost/basic-auth', {useMongoClient: true})
+  .then(() => {
+    console.log('Connected to Mongo!')
+  }).catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({ 
+  secret: "our-passport-local-strategy-app", 
+  resave: true, 
+  saveUninitialized: true,
+}));
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+app.use(flash()); 
+passport.use(new LocalStrategy({
+ passReqToCallback: true }, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+app.use(passport.initialize()); 
+app.use(passport.session());
 
 // Express View engine setup
 
